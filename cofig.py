b1ce03cb24659984f050290625036a48dbca583b
GamingBeast1 <116881154+GamingBeast1@users.noreply.github.com>
2023-05-21 12:18:24 +0530

    Create cofig.py

---
+++ cofig.py
@@ -0,0 +1,48 @@
+# config.py
+TOKEN = "<6227804428:AAGaK9tls3NgqbDm4edCGikyIt01YvJEa_o>"
+ADMINS = []  # List of user IDs of the bot admins
+
+API_HASH = "6590905e28c61bca1ad5e83de9853cf8"  # Your Telegram API hash
+BOT_TOKEN = "6227804428:AAGaK9tls3NgqbDm4edCGikyIt01YvJEa_o"  # Your Telegram bot token
+API_ID = "25705219"  # Your Telegram API ID
+
+PICS = []  # List of file IDs for pictures to be used in the bot
+
+CHANNELS = []  # List of channel IDs the bot will operate in
+
+AUTH_USERS = []  # List of user IDs authorized to access certain bot features
+
+AUTH_CHANNEL = None  # Channel ID for authorization purposes
+
+SUPPORT_CHAT_ID = "-1001949369130"  # Chat ID of the support chat
+
+DATABASE_URI = "mongodb+srv://Lalli:ekam@moviesbot.7as7avv.mongodb.net/?retryWrites=true&w=majority"  # URI of your database (e.g., MongoDB, PostgreSQL)
+
+DATABASE_NAME = "moviesbot"  # Name of the database
+
+SHORTLINK_URL = "TinyFy.in"  # URL for the URL shortener feature
+
+URL_SHORTENER_API = "dd6eaae867ac95d21f4d7031fef4d82261a3fabd"  # URL shortener API variable
+
+IS_SHORTLINK = True  # Boolean indicating if URL shortening is enabled or not
+
+GRP_LNK = "https://t.me/MoviesGalaxy120"  # Group link
+
+CHNL_LNK = "https://t.me/MoviesGalaxy01"  # Channel link
+
+LOG_CHANNEL = "-1001976241801"  # Channel ID for logging purposes
+
+MELCOW_VID = "http://telegra.ph/file/eec4b77900a67f24991ea.mp4"  # Video file ID for Melcow video
+
+SPELL_IMG = "https://telegra.ph/file/c7ef09b9cca4d983cdac8.jpg"  # Image file ID for spell image
+
+AUTH_CHANNEL = -1001929814421 # Channel ID for authorization purposes
+
+CUSTOM_FILE_CAPTION = "▫️File Name- {Filename}
+
+▫️File Size- {Filesize}
+
+
+Channel- @Moviesgalaxy01
+Group- @Moviesgalaxy120
+"  # Custom file caption variable
